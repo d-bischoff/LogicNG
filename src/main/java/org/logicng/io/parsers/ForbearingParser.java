a791321054b0ee6f7f25e198cda426beabc69da9
Daniel Bischoff <d.bischoff@uni-tuebingen.de>
2016-06-09 11:44:43 +0200

    added a forbearing parser - it allows variabels to start with numbers, use + - / as well as & ~ |.

---
+++ src/main/java/org/logicng/io/parsers/ForbearingParser.java
@@ -0,0 +1,83 @@
+package org.logicng.io.parsers;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BailErrorStrategy;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Daniel Bischoff on 14.01.2016. Copyright by Daniel Bischoff
+ */
+public final class ForbearingParser {
+
+  private final FormulaFactory f;
+  private final org.symcom.svm_proto.model.logic.parsers.ForbearingParserLexer lexer;
+  private final org.symcom.svm_proto.model.logic.parsers.ForbearingParserParser parser;
+
+  /**
+   * Constructs a new parser.
+   *
+   * @param f the formula factory
+   */
+  public ForbearingParser(final FormulaFactory f) {
+    this.f = f;
+    ANTLRInputStream input = new ANTLRInputStream();
+    this.lexer = new org.symcom.svm_proto.model.logic.parsers.ForbearingParserLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(this.lexer);
+    this.parser = new org.symcom.svm_proto.model.logic.parsers.ForbearingParserParser(tokens);
+    this.parser.setFormulaFactory(f);
+    this.lexer.removeErrorListeners();
+    this.parser.removeErrorListeners();
+    this.parser.setErrorHandler(new BailErrorStrategy());
+  }
+
+  /**
+   * Parses and returns a given string.
+   *
+   * @param in a string
+   * @return the {@link Formula} representation of this string
+   * @throws ParserException if the string was not a valid formula
+   */
+  public Formula parse(final String in) throws ParserException {
+    if (in == null || in.isEmpty())
+      return f.verum();
+    return this.parse(new ByteArrayInputStream(in.getBytes()));
+  }
+
+  /**
+   * Parses and returns a given input stream.
+   *
+   * @param inputStream an input stream
+   * @return the {@link Formula} representation of this stream
+   * @throws ParserException if there was a problem with the input stream
+   */
+  @SuppressWarnings("WeakerAccess")
+  public Formula parse(InputStream inputStream) throws ParserException {
+    try {
+      ANTLRInputStream input = new ANTLRInputStream(inputStream);
+      this.lexer.setInputStream(input);
+      CommonTokenStream tokens = new CommonTokenStream(this.lexer);
+      this.parser.setInputStream(tokens);
+      return this.parser.formula().f;
+    } catch (IOException e) {
+      throw new ParserException("IO exception when parsing the formula", e);
+    } catch (ParseCancellationException e) {
+      throw new ParserException("Parse cancellation exception when parsing the formula", e);
+    } catch (LexerException e) {
+      throw new ParserException("Lexer exception when parsing the formula.", e);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return this.getClass().getSimpleName();
+  }
+}
+
+
---
+++ src/main/resources/parser/ForbearingParser.g4
@@ -0,0 +1,73 @@
+grammar ForbearingParser;
+
+@parser::header {
+package org.symcom.svm_proto.model.logic.parsers;
+
+import java.util.LinkedHashSet;
+import org.logicng.formulas.*;
+}
+
+@members {
+private FormulaFactory f;
+
+public void setFormulaFactory(final FormulaFactory f) {
+  this.f = f;
+}
+}
+
+@lexer::header {
+package org.symcom.svm_proto.model.logic.parsers;
+
+import org.logicng.formulas.FormulaFactory;
+}
+
+formula returns [Formula f]
+  : EOF {$f = f.verum();}
+  | equiv EOF {$f = $equiv.f;}
+  ;
+
+constant returns [Formula f]
+  : TRUE  {$f = f.verum();}
+  | FALSE {$f = f.falsum();}
+  ;
+
+simp returns [Formula f]
+  :	VARIABLE      {$f = f.variable($VARIABLE.text);}
+  |	constant      {$f = $constant.f;}
+  | LBR equiv RBR {$f = $equiv.f;};
+
+lit returns [Formula f]
+  :	NOT a = lit {$f = f.not($a.f);}
+  |	simp        {$f = $simp.f;};
+
+conj returns [Formula f]
+@init{LinkedHashSet<Formula> literals = new LinkedHashSet<>(); }
+	:	a = lit {literals.add($a.f);} (AND b = lit {literals.add($b.f);})* {$f = f.and(literals);};
+
+disj returns [Formula f]
+@init{LinkedHashSet<Formula> conjunctions = new LinkedHashSet<>();}
+  :	a = conj {conjunctions.add($a.f);} (OR b = conj {conjunctions.add($b.f);})* {$f = f.or(conjunctions);};
+
+impl returns [Formula f]
+@init{Formula[] operands = new Formula[2];}
+  :	a = disj {operands[0] =$a.f;} (IMPL b = impl {operands[1] = $b.f;})? {$f = operands[1] == null ? operands[0] : f.implication(operands[0], operands[1]);};
+
+equiv returns [Formula f]
+@init{Formula[] operands = new Formula[2];}
+  :	a = impl {operands[0] =$a.f;} (EQUIV b = equiv {operands[1] = $b.f;})? {$f = operands[1] == null ? operands[0] : f.equivalence(operands[0], operands[1]);};
+
+VARIABLE : [A-Za-z0-9_@]+;
+TRUE     : '$true';
+FALSE    : '$false';
+LBR      : '(';
+RBR      : ')';
+NOT      : '-'
+         | '~';
+AND      : '+'
+         | '&';
+OR       : '/'
+         | '|';
+IMPL     : '=>';
+EQUIV    : '<=>';
+WS       : [ \t\r\n;]+ -> skip ;
+
