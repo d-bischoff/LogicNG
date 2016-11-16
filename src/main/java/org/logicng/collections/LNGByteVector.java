8ea6220f36fbabb3b3b5d48be893633f0187aabd
BISCDAN <ladimap123>
2016-11-16 17:06:56 +0100

    back to the roots (going back to how LNG organized things) forgot a thing

--- src/main/java/org/logicng/collections/LNGByteVector.java
+++ src/main/java/org/logicng/collections/LNGByteVector.java
@@ -123,18 +123,6 @@ public final class LNGByteVector {
   }
 
   /**
-   * Ensures that this vector has the given size.  If not - the size is doubled and the old elements are copied.
-   * @param newSize the size to ensure
-   */
-  private void ensure(final int newSize) {
-    if (newSize >= this.elements.length) {
-      final byte[] newArray = new byte[Math.max(newSize, this.size * 2)];
-      System.arraycopy(this.elements, 0, newArray, 0, this.size);
-      this.elements = newArray;
-    }
-  }
-
-  /**
    * Pushes an element and assumes that there is enough space on the vector.
    * @param element the element to push
    * @throws ArrayIndexOutOfBoundsException if there was not enough space on the vector
@@ -237,6 +225,18 @@ public final class LNGByteVector {
    */
   public byte[] toArray() {
     return Arrays.copyOf(this.elements, this.size);
+  }
+
+  /**
+   * Ensures that this vector has the given size.  If not - the size is doubled and the old elements are copied.
+   * @param newSize the size to ensure
+   */
+  private void ensure(final int newSize) {
+    if (newSize >= this.elements.length) {
+      final byte[] newArray = new byte[Math.max(newSize, this.size * 2)];
+      System.arraycopy(this.elements, 0, newArray, 0, this.size);
+      this.elements = newArray;
+    }
   }
 
   @Override
